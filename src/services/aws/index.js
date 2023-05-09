a8a31dbd83cf66373959c030fa2e2b185f94fef7
nihalwashere <nihal.kaul@sjsu.edu>
2023-05-08 18:01:57 -0700

    fix: minor

--- src/services/aws/index.js
+++ src/services/aws/index.js
@@ -1,8 +1,5 @@
 const AWS = require("aws-sdk");
 
-// const bucket = "cherry-staging-images"; // the bucketname without s3://
-// const photo = "mocking_bird.jpeg"; // the name of file
-
 new AWS.Config({
   accessKeyId: "",
   secretAccessKey: ""
